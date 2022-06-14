8ea3fd79efb14c1a3281ecad4a2fb77e9d9dadbf
kaledai069 <parzival.cooler@gmail.com>
2022-06-14 22:58:45 +0545

    assignment 4 submission

--- Coursera Assignment - 4/nnCostFunction.m
+++ Coursera Assignment - 4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -62,30 +62,58 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% first, calculating the hypothetical values for each row of image data into the
+% corresponding digit value 1-10,
+% output: predicted_y is the hypothetical value vector of dim 5000 x 1
+X = [ones(m, 1) X];
+act_layer1 = sigmoid(X * Theta1');
+act_layer1 = [ones(m, 1) act_layer1];
+predicted_y = sigmoid(act_layer1 * Theta2');
+%[_ predicted_y] = max(final_layer, [], 2);
 
+y = createVecNum(y, num_labels);
+J = -(y .* log(predicted_y) + (1 - y) .* log(1 - predicted_y)) / m;
+J = sum(J(:));
 
+first_reg_value = sum((Theta1(:, 2:end) .* Theta1(:, 2:end))(:));
+second_reg_value = sum((Theta2(:, 2:end) .* Theta2(:, 2:end))(:));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+J = J + lambda / (2*m) * (first_reg_value + second_reg_value);
 
 % -------------------------------------------------------------
 
 % =========================================================================
+for i = 1:m,
+  a_1 = X(i, :)';
+  a_2 = [1 ; sigmoid(Theta1 * a_1)];
+  a_3 = sigmoid(Theta2 * a_2);
+  del_3 = a_3 - y(i, :)';
+  del_2 = (Theta2' * del_3)(2:end) .* sigmoidGradient(Theta1 * a_1);
+  Theta1_grad = Theta1_grad + del_2 * a_1';
+  Theta2_grad = Theta2_grad + del_3 * a_2';
+endfor
+
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + lambda * Theta1(:, 2:end);
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + lambda * Theta2(:, 2:end);
+
+Theta1_grad = Theta1_grad / m;
+Theta2_grad = Theta2_grad / m;
+
+
 
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
 
+end
 
+
+% convert m x 1 vector into m x k matrix transforming [1] label value to
+% [1 x k] row vector corresponding to index high otherwise 0
+function new_vec = createVecNum(vec, no_classes)
+  m = size(vec, 1);
+  temp_vec = zeros(m, no_classes);
+  for idx = 1:m,
+    temp_vec(idx, vec(idx)) = 1;
+  endfor
+  new_vec = temp_vec;
 end
